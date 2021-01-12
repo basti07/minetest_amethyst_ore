2b1182a362a92ae35938a2239478430d0dc1e89e
basti07 <sebastian@planetorange.biz>
2021-01-12 19:07:49 +0100

    main

--- init.lua
+++ init.lua
@@ -1,21 +1,26 @@
 local S = default.get_translator
 
+minetest.register_craftitem("amethyst:amethyst_ingot", {
+	description = S("Amethyst Ingot"),
+	inventory_image = "amethyst_ingot.png"
+})
+
 minetest.register_node("amethyst:amethyst", {
 	description = S("Amethyst Ore"),
 	tiles = {"mineral_amethyst.png"},
 	groups = {cracky = 2, level = 2},
 	sounds = default.node_sound_stone_defaults(),
 	light_source = 3,
+	drop = "amethyst:amethyst_ingot",
 })
-
 
 minetest.register_ore({
 		ore_type       = "scatter",
 		ore            = "amethyst:amethyst",
 		wherein        = "default:stone",
-		clust_scarcity = 45 * 45 * 45,
-		clust_num_ores = 30,
-		clust_size     = 5,
+		clust_scarcity = 40 * 40 * 40,
+		clust_num_ores = 5,
+		clust_size     = 8,
 		y_max          = -2048,
 		y_min          = -31000,
 	})
@@ -23,39 +28,82 @@ minetest.register_ore({
 	
 if minetest.get_modpath("3d_armor") then
 	armor:register_armor("amethyst:helmet_amethyst", {
-		description = S("Fast Helmet"),
-		inventory_image = "3d_armor_helmet_amethyst.png",
+		description = S("Amethyst Helmet"),
+		inventory_image = "amethyst_helmet_amethyst_preview.png",
 		armor_groups = {fleshy=10},
 		groups = {armor_head=1, armor_heal=3, armor_use=50},
 		damage_groups = {cracky=1, snappy=2, choppy=2, crumbly=1, level=2},
 	})
+	
+	
 
 	armor:register_armor("amethyst:chestplate_amethyst", {
-		description = S("Fast chestplate"),
-		inventory_image = "3d_armor_inv_chestplate_amethyst.png",
+		description = S("Amethyst chestplate"),
+		inventory_image = "amethyst_chestplate_amethyst_preview.png",
 		armor_groups = {fleshy=10},
 		groups = {armor_torso=1, armor_heal=5, armor_use=50},
 		damage_groups = {cracky=2, snappy=3, choppy=2, crumbly=1, level=2},
 	})
+	
+	
 
 
 	armor:register_armor("amethyst:leggings_amethyst", {
-		description = S("Fast Leggigns"),
-		inventory_image = "3d_armor_inv_leggings_amethyst.png",
+		description = S("Amethyst Leggigns"),
+		inventory_image = "amethyst_leggins_amethyst_preview.png",
 		armor_groups = {fleshy=10},
 		groups = {armor_legs=1, armor_heal=5, armor_use=800},
 		damage_groups = {cracky=2, snappy=3, choppy=2, crumbly=1, level=2},
 	})
+	
+	
 
 	armor:register_armor("amethyst:boots_amethyst", {
-		description = S("Fast Boots"),
-		inventory_image = "3d_armor_inv_boots_amethyst.png",
+		description = S("Amethyst Boots"),
+		inventory_image = "amethyst_boots.png",
 		armor_groups = {fleshy=10},
 		groups = {armor_feet=1, armor_heal=5, armor_use=800, physics_speed = 1, puts_out_fire = 5},
 		damage_groups = {cracky=2, snappy=3, choppy=2, crumbly=1, level=2},
 	})
+	
+	
 end
-	
 
-	
 
+minetest.register_craft({
+		output = "amethyst:helmet_amethyst",
+		recipe = {
+			{"amethyst:amethyst_ingot", "amethyst:amethyst_ingot", "amethyst:amethyst_ingot"},
+			{"amethyst:amethyst_ingot", "", "amethyst:amethyst_ingot"}
+		}
+	})
+
+
+minetest.register_craft({
+		output = "amethyst:chestplate_amethyst",
+		recipe = {
+			{"amethyst:amethyst_ingot", "", "amethyst:amethyst_ingot"},
+			{"amethyst:amethyst_ingot", "amethyst:amethyst_ingot", "amethyst:amethyst_ingot"},
+			{"amethyst:amethyst_ingot", "amethyst:amethyst_ingot", "amethyst:amethyst_ingot"}
+		}
+	})
+
+
+
+minetest.register_craft({
+		output = "amethyst:leggings_amethyst",
+		recipe = {
+			{"amethyst:amethyst_ingot", "amethyst:amethyst_ingot", "amethyst:amethyst_ingot"},
+			{"amethyst:amethyst_ingot", "", "amethyst:amethyst_ingot"},
+			{"amethyst:amethyst_ingot", "", "amethyst:amethyst_ingot"}
+		}
+	})
+
+
+minetest.register_craft({
+		output = "amethyst:boots_amethyst",
+		recipe = {
+			{"amethyst:amethyst_ingot", "", "amethyst:amethyst_ingot"},
+			{"amethyst:amethyst_ingot", "", "amethyst:amethyst_ingot"}
+		}
+	})
